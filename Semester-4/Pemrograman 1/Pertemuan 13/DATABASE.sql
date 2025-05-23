0b1a59ed696a3c5a96c56b9fb1ecfdd3bd9fb6e6
rafliaryansyah <raflyaryansha86@gmail.com>
2025-05-23 20:46:31 +0700

    `task`: add database.sql docs editor (S4_P1_PER13)

---
+++ Semester-4/Pemrograman 1/Pertemuan 13/DATABASE.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE p13_users;
+
+USE p13_users;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NULL,
+    password VARCHAR(255) NULL
+);
+
+INSERT INTO users (username, password) VALUES
+('admin', 'admin'),
+('user1', 'user1'),
