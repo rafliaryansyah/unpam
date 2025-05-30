78f42be22e09393e2b134526b19aedd5184c42d0
rafliaryansyah <raflyaryansha86@gmail.com>
2025-05-30 15:35:56 +0700

    `task`: pemrograman 1 quest 2 stok barang (S4_P1_PER14_15)

---
+++ Semester-4/Pemrograman 1/Pertemuan 14 & 15/P15/src/DatabaseConnection.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author rafli
+ */
+public class DatabaseConnection {
+    public static Connection connect() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/p15_management_product", "root", ""); // Ubah sesuai
+            System.out.println("Koneksi berhasil");
+        } catch (Exception e) {
+            System.out.println("Koneksi gagal: " + e.getMessage());
+        }
+        return conn;
+    }
+}
---
+++ Semester-4/Pemrograman 1/Pertemuan 14 & 15/P15/src/p15/FormStokBarang.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package p15;
+
+/**
+ *
+ * @author rafli
+ */
+public class FormStokBarang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
